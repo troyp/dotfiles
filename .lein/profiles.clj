5c206b392f30a18a3531667e60babb5579c45eb9
Troy Pracy <troyp7@gmail.com>
2016-07-04 18:37:28 +1000

    new file

---
+++ .lein/profiles.clj
@@ -0,0 +1,16 @@
+;; {:user {:plugins
+;;         [[lein-catnip "0.5.1"]
+;;          [org.bodil/lein-noderepl "0.1.11"]
+;;          ]
+;;         }
+;;  }
+{:repl {:plugins
+        [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
+         [refactor-nrepl "2.0.0-SNAPSHOT"]
+         ]
+        :dependencies
+        [[alembic "0.3.2"]
+         [org.clojure/tools.nrepl "0.2.12"]
+         ]
+        }
+ }
