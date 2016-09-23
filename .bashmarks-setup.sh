5fb62a942703e109a05ab5160686d7b202b337e3
Troy Pracy <troyp7@gmail.com>
2016-09-24 02:54:52 +1000

    move function rename/alias commands inside conditional

--- .bashmarks-setup.sh
+++ .bashmarks-setup.sh
@@ -1,8 +1,9 @@
 
-if [ -f /opt/scripts/bashmarks.sh ]; then source /opt/scripts/bashmarks.sh; fi
-
-rename-function s dirsav  # bashmark save
-alias-function  g dirgo   # bashmark go
-rename-function p dirpr   # bashmark print
-rename-function d dirdel  # bashmark delete
-rename-function l dirls   # bashmark list
+if [ -f /opt/scripts/bashmarks.sh ]; then
+    source /opt/scripts/bashmarks.sh
+    rename-function s dirsav  # bashmark save
+    alias-function  g dirgo   # bashmark go
+    rename-function p dirpr   # bashmark print
+    rename-function d dirdel  # bashmark delete
+    rename-function l dirls   # bashmark list
+fi
