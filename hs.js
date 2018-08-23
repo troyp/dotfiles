b18732035562c28b2d324ff28466f0c39c48e43b
Troy Pracy <troyp7@gmail.com>
2018-08-23 13:49:35 +1000

    Remove example items

--- hs.js
+++ hs.js
@@ -4,8 +4,4 @@ item({desc: 'top level menu'}, function () {
 
     item({key: 'p', desc: 'gPick colour picker', wd: './', cmd: 'gpick && exit'})
     item({key: 's', desc: 'XFCE screenshot'    , wd: './', cmd: 'xfce4-screenshooter && exit'})
-
-    // a description can be used to clarify the command intent
-    item({key: 'a', desc: 'access logs', cmd: 'less +F /var/log/apache2/access.log'})
-    item({key: 'e', desc: 'error logs', cmd: 'less +F /var/log/apache2/error.log'})
 })
