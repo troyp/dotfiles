d1886f18957afcc9f8008af4aa41d1ac5f6bbd07
Troy Pracy <troyp7@gmail.com>
2018-08-23 13:49:48 +1000

    Add items for quitting with q or Escape

--- hs.js
+++ hs.js
@@ -2,6 +2,8 @@ var item = require('hotshell').item
 
 item({desc: 'top level menu'}, function () {
 
-    item({key: 'p', desc: 'gPick colour picker', wd: './', cmd: 'gpick && exit'})
-    item({key: 's', desc: 'XFCE screenshot'    , wd: './', cmd: 'xfce4-screenshooter && exit'})
+    item({key: 'p',  desc: 'gPick colour picker', wd: './', cmd: 'gpick && exit'})
+    item({key: 's',  desc: 'XFCE screenshot'    , wd: './', cmd: 'xfce4-screenshooter && exit'})
+    item({key: 'q',  desc: 'quit'               , wd: './', cmd: 'pkill --signal SIGTERM hs'})
+    item({key: "", desc: 'quit'               , wd: './', cmd: 'pkill --signal SIGTERM hs'})
 })
