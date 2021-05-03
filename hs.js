9c423f486ae0d8f78e3d9911b6fc0f6148351e05
Troy Pracy <troyp7@gmail.com>
2021-05-03 14:29:27 +0000

    Change p to gPick prefix, add r (reading list)

--- hs.js
+++ hs.js
@@ -1,17 +1,24 @@
 var item = require('hotshell').item
 
-item({desc: 'top level menu'}, function () {
-
-    item({key: 'p',  desc: 'gPick colour picker', wd: './', cmd: 'gpick && exit'})
-    item(
-        {key: 'k',  desc: 'KDE', wd: './'},
-        function() {
-            item({key: 'k',  desc: 'KDE system settings', cmd: 'systemsettings5 2>/dev/null && exit'})
-            item({key: 'q',  desc: 'quit'               , cmd: 'pkill --signal SIGTERM hs'})
-            item({key: "", desc: 'quit'               , cmd: 'pkill --signal SIGTERM hs'})
-        }
-        )
-    item({key: 's',  desc: 'XFCE screenshot'    , wd: './', cmd: 'xfce4-screenshooter && exit'})
-    item({key: 'q',  desc: 'quit'               , wd: './', cmd: 'pkill --signal SIGTERM hs'})
-    item({key: "", desc: 'quit'               , wd: './', cmd: 'pkill --signal SIGTERM hs'})
-})
+item(
+    {desc: 'top level menu'},
+    function () {
+        item(
+            {key: 'p',  desc: 'gPick colour picker', wd: './'},
+            function() {
+                item({key: 'p', desc: 'Pick colour', cmd: 'gpick -p && pkill --signal SIGTERM hs'});
+                item({key: 'w', desc: 'gPick window', cmd: 'setsid gpick'});
+            }
+        );
+        item(
+            {key: 'k',  desc: 'KDE', wd: './'},
+            function() {
+                item({key: 'k',  desc: 'KDE system settings', cmd: 'kstart5 systemsettings5 2>/dev/null &'});
+                item({key: 'q',  desc: 'quit'               , cmd: 'pkill --signal SIGTERM hs'});
+                item({key: "", desc: 'quit'               , cmd: 'pkill --signal SIGTERM hs'});
+            });
+        item({key: 'r',  desc: 'reading list'       , wd: './', cmd: 'lxterm -e "bash --norc -c rlistrun" &'})
+        item({key: 's',  desc: 'XFCE screenshot'    , wd: './', cmd: 'setsid xfce4-screenshooter &'});
+        item({key: 'q',  desc: 'quit'               , wd: './', cmd: 'pkill --signal SIGTERM hs'});
+        item({key: "", desc: 'quit'               , wd: './', cmd: 'pkill --signal SIGTERM hs'});
+    });
